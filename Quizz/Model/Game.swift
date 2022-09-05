012b6a85dcadd617482e85e4bcdb4867798df0d2
Rabat martin <martin.rabat@gmail.com>
2022-09-05 08:39:37 +0200

    first commit

---
+++ Quizz/Model/Game.swift
@@ -0,0 +1,56 @@
+//
+//  Game.swift
+//  Quizz
+//
+//  Created by Martin Celavie on 26/08/2022.
+//
+import Foundation
+
+
+class Game {
+    var score = 0
+
+    private var questions = [Question]()
+    private var currentIndex = 0
+
+    var state: State = .ongoing
+
+    enum State {
+        case ongoing, over
+    }
+
+    var currentQuestion: Question {
+        return questions[currentIndex]
+    }
+
+    func refresh() {
+        score = 0
+        currentIndex = 0
+        state = .over
+        
+        QuestionManager.shared.get { (questions) in
+            self.questions = questions
+            self.state = .ongoing
+        }
+    }
+
+    func answerCurrentQuestion(with answer: Bool) {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
+            score += 1
+        }
+        goToNextQuestion()
+    }
+
+    private func goToNextQuestion() {
+        let questionsTotal = questions.count - 1
+        if currentIndex < questionsTotal {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+
+    private func finishGame() {
+        state = .over
+    }
+}
---
+++ Quizz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quizz
+//
+//  Created by Martin Celavie on 26/08/2022.
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
