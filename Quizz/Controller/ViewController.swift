adb1343284e1ab3fdef4f64785499db7a90afa4d
Rabat martin <martin.rabat@gmail.com>
2022-09-05 17:52:59 +0200

    update from cours

--- Quizz/Controller/ViewController.swift
+++ Quizz/Controller/ViewController.swift
@@ -9,12 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionView: QuestionView!
+    
+    var game = Game()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let name = Notification.Name(rawValue: "QuestionsLoaded")
+        NotificationCenter.default.addObserver(self, selector: #selector(questionsLoaded), name: name, object: nil)
+        startNewGame()
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragQuestionView(_:)))
+        questionView.addGestureRecognizer(<#T##gestureRecognizer: UIGestureRecognizer##UIGestureRecognizer#>)
     }
 
-
+    
+    @objc func questionsLoaded() {
+        newGameButton.isHidden = false
+        activityIndicator.isHidden = true
+        
+        questionView.title = game.currentQuestion.title
+        
+    }
+    @IBAction func didTapNewGameButton() {
+        startNewGame()
+    }
+    
+    private func startNewGame() {
+        activityIndicator.isHidden = false
+        newGameButton.isHidden = true
+        
+        questionView.title = " Loading ..."
+        questionView.style = .standart
+        
+        scoreLabel.text = "0 / 10"
+        
+        game.refresh()
+    }
+    func dragQuestionView(_ sender: UIPanGestureRecognizer)
+    {
+        if game.state == .ongoing {
+            switch sender.state {
+            case .began, .changed:
+                transformQuestionViewWith(gesture: sender)
+            case .cancelled, .ended:
+                answerquestion()
+            default:
+                break
+            }
+        }
+    }
+    private func transformQuestionViewWith(gesture: UIPanGestureRecognizer)
+    {
+        
+    }
+    
+    private func answerquestion()
+    {
+        
+    }
 }
 
 
--- Quizz/View/QuestionView.swift
+++ Quizz/View/QuestionView.swift
@@ -9,7 +9,39 @@ import UIKit
 
 class QuestionView: UIView {
     
-    @IBOutlet private var label: UILabel?
-    @IBOutlet private var icon: UIImageView?
+    @IBOutlet private var label: UILabel!
+    
+    @IBOutlet private var icon: UIImageView!
 
+    enum Style {
+        case correct, incorrect, standart
+    }
+    
+    var title = "" {
+        didSet {
+            label.text = title
+        }
+    }
+    
+    private func setStyle (_ style: Style) {
+        switch style {
+        case .correct:
+            backgroundColor = UIColor(red: 200/255.0, green: 236/255.0, blue: 160/255.0, alpha: 1)
+            icon.image = UIImage(named: "Icon Correct")
+            icon.isHidden = false
+        case .incorrect:
+            backgroundColor = UIColor(red: 243/255.0, green: 135/255.0, blue: 148/255.0, alpha: 1)
+            icon.image = UIImage(named: "Icon Error")
+            icon.isHidden = false
+        case .standart:
+            backgroundColor = UIColor(red: 191/255.0, green: 196/255.0, blue: 201/255.0, alpha: 1)
+            icon.isHidden = true
+        }
+    }
+    
+    var style: Style = .standart {
+        didSet {
+                setStyle(style)
+        }
+    }
 }
