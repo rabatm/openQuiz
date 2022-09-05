74eac996d92ef07db7b25715374469eb65893c80
Martin Celavie <martincelavie@Martins-MacBook-Air.local>
2022-09-05 16:26:13 +0200

    update from macBookAri

---
+++ Quizz/View/QuestionView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  Quizz
+//
+//  Created by Martin Celavie on 05/09/2022.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
